88b2477410f9d7ea78bed1fcfefed3859b0103c6
Kiranjeet <heykiranthakur@gmail.com>
2023-05-20 23:11:28 +0530

    gender

--- controller/user.js
+++ controller/user.js
@@ -172,7 +172,7 @@ module.exports.createUser = function (req, res) {
             })
         }
         else {
-            const { username, email} = req.body
+            const { username, email,gender} = req.body
             // FUNCTION TO MAKE THE EMAIL ENTER ONLY ONCE IN THE DATABASE 
             let user_exist_true = await User.findOne({ email: email ,verified:true});
             if (user_exist_true) {
@@ -202,6 +202,7 @@ module.exports.createUser = function (req, res) {
                     username: req.body.username,
                     email: req.body.email,
                     avatar: avatar,
+                    gender:req.body.gender,
                     rollNumber:req.body.rollNumber,
                     verified:false
 
