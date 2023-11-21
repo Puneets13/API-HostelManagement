f57f9b6d07481cca2c29be6c473734e1c536a5ab
Kiranjeet <heykiranthakur@gmail.com>
2023-11-21 23:21:22 +0530

    90876

--- models/diet.js
+++ models/diet.js
@@ -1,18 +1,13 @@
 const mongoose = require('mongoose');
-const mealSchema = new mongoose.Schema({
-  _id:mongoose.Schema.Types.ObjectId,
-  date: {
-    type: String,
-    // unique:true,
-   },
-   breakfast: Number, // 0 for false, 1 for true
-   lunch: Number,     // 0 for false, 1 for true
-   dinner: Number,    //
-//
-});
 
+const mealSchema=new mongoose.Schema({
+    date: Date,
+    breakfast:Number,
+    lunch: Number,
+    dinner:Number,
+})
 
-const dietRecords= new mongoose.Schema({
+const dietSchema = new mongoose.Schema({
     _id:mongoose.Schema.Types.ObjectId,
     roomNumber: {
         type: String,
