fc346862f895e40f3b5843475737584d9402fac9
Puneet Singh <sidhupuneet115@gmail.com>
2024-04-01 00:46:34 +0530

    1 april 2023

--- controller/user.js
+++ controller/user.js
@@ -668,7 +668,8 @@ module.exports.loginUser = function (req, res) {
                         }
                     }
 
-
+// Since the Admin roles are not present in the Hostel Records so they will return null.
+// handling them in this way.
                     const emailToHostelMapping = {
                         "bh7@nitj.ac.in": { hostelName: "Boys Hostel 7", roomNumber: "000" },
                         "bh7E@nitj.ac.in": { hostelName: "Boys Hostel 7E", roomNumber: "000" },
@@ -694,76 +695,8 @@ module.exports.loginUser = function (req, res) {
                             roomNumber: hostelDetails.roomNumber
                         };
                         } 
-                        
-                    // if(req.body.email = "bh7@nitj.ac.in"){
-                    //     hostel1={
-                    //         hostelName:"Boys Hostel 7",
-                    //         roomNumber:"000"
-                    //     }  
-                    // }
-                    // if(req.body.email = "bh7E@nitj.ac.in"){
-                    //     hostel1={
-                    //         hostelName:"Boys Hostel 7E",
-                    //         roomNumber:"000"
-                    //     }  
-                    // }
-                    // if(req.body.email = "bh3@nitj.ac.in"){
-                    //     hostel1={
-                    //         hostelName:"Boys Hostel 3",
-                    //         roomNumber:"000"
-                    //     }  
-                    // }
-                    // if(req.body.email = "bh4@nitj.ac.in"){
-                    //     hostel1={
-                    //         hostelName:"Boys Hostel 4",
-                    //         roomNumber:"000"
-                    //     }  
-                    // }
-                    // if(req.body.email = "bh6@nitj.ac.in"){
-                    //     hostel1={
-                    //         hostelName:"Boys Hostel 6",
-                    //         roomNumber:"000"
-                    //     }  
-                    // }
-                    // if(req.body.email = "mbh.B@nitj.ac.in"){
-                    //     hostel1={
-                    //         hostelName:"Mega Boys Hostel B",
-                    //         roomNumber:"000"
-                    //     }  
-                    // }
-                    // if(req.body.email = "mbh.A@nitj.ac.in"){
-                    //     hostel1={
-                    //         hostelName:"Mega Boys Hostel A",
-                    //         roomNumber:"000"
-                    //     }  
-                    // }
-                    // if(req.body.email = "mbh.F@nitj.ac.in"){
-                    //     hostel1={
-                    //         hostelName:"Mega Boys Hostel F",
-                    //         roomNumber:"000"
-                    //     }  
-                    // }
-                    // if(req.body.email = "mgh.A@nitj.ac.in"){
-                    //     hostel1={
-                    //         hostelName:"Girls Hostel A",
-                    //         roomNumber:"000"
-                    //     }  
-                    // }
 
-                    // if(req.body.email = "mgh.B@nitj.ac.in"){
-                    //     hostel1={
-                    //         hostelName:"Girls Hostel B",
-                    //         roomNumber:"000"
-                    //     }  
-                    // }
-                    // if(req.body.email = "mgh@nitj.ac.in"){
-                    //     hostel1={
-                    //         hostelName:"Mega Girls Hostel",
-                    //         roomNumber:"000"
-                    //     }  
-                    // }
-
-
+                   
                     if(hostel!=null){
                         hostel1={
                             roomNumber:hostel.roomNumber,
